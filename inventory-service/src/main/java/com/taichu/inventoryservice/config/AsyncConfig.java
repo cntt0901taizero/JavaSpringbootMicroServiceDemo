a98dcc272470369b4be7b94377b876b06d3bb627
anbinh-dbd <thetaichu@gmail.com>
2025-02-11 18:20:04 +0700

    t

---
+++ inventory-service/src/main/java/com/taichu/inventoryservice/config/AsyncConfig.java
@@ -0,0 +1,10 @@
+// package com.taichu.inventoryservice.config;
+
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.scheduling.annotation.EnableAsync;
+
+// @Configuration
+// @EnableAsync
+// public class AsyncConfig {
+//     // kich hoat Asynchonrus
+// }
--- inventory-service/src/main/java/com/taichu/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/taichu/inventoryservice/service/InventoryService.java
@@ -6,10 +6,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
 @Service
@@ -20,6 +22,7 @@ public class InventoryService {
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
+    // @Async
     // @SneakyThrows
     public List<InventoryResponse> isInStock(List<String> skuCode) {
         log.info("wait to started");
@@ -30,12 +33,11 @@ public class InventoryService {
         }
         log.info("wait ended");
 
-        return inventoryRepository.findBySkuCodeIn(skuCode).stream()
-            .map(inventory ->
-               InventoryResponse.builder()
-                       .skuCode(inventory.getSkuCode())
-                       .isInStock(inventory.getQuantity() > 0)
-                       .build()
+        return inventoryRepository.findBySkuCodeIn(skuCode).stream().map(inventory ->
+            InventoryResponse.builder()
+                    .skuCode(inventory.getSkuCode())
+                    .isInStock(inventory.getQuantity() > 0)
+                    .build()
         ).collect(Collectors.toList());
     }
 }
